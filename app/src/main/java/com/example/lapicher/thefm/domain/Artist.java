00d96fe6a11991d89d6faef06c295414b2bdd327
Nataly Contreras <natalyjanethcon@gmail.com>
2015-08-14 17:04:31 -0500

    se quita boton y se cambia por un recycledview, se crea CardView

---
+++ app/src/main/java/com/example/lapicher/thefm/domain/Artist.java
@@ -0,0 +1,20 @@
+package com.example.lapicher.thefm.domain;
+
+/**
+ * Created by Nataly on 14/08/2015.
+ */
+public class Artist {
+    String name;
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/com/example/lapicher/thefm/ui/HypedArtistsFragment.java
+++ app/src/main/java/com/example/lapicher/thefm/ui/HypedArtistsFragment.java
@@ -3,6 +3,8 @@ package com.example.lapicher.thefm.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +16,26 @@ import com.example.lapicher.thefm.R;
  */
 public class HypedArtistsFragment extends Fragment {
 
+    public static final String LOG_TAG=HypedArtistsFragment.class.getName();
 
+    private RecyclerView mHypedArtistList;
+    public static final int NUM_COLUMNS=2;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        //View root=inflater.inflate(R.layout.fragment_hyped_artists,container,false);
+
         View root = inflater.inflate(R.layout.fragment_hyped_artists,container,false);
+        /*
+            Encuentra en el fragmento el RecycledView
+         */
+        mHypedArtistList=(RecyclerView)root.findViewById(R.id.hyped_artists_list);
 
         return root;
+    }
+    private void setupArtistsList()
+    {
+        mHypedArtistList.setLayoutManager(new GridLayoutManager(getActivity(),NUM_COLUMNS));
     }
 }
