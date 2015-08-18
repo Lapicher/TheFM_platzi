e98eb54151614a1ae7d20533f9a40cd98f99a1fa
Nataly Contreras <natalyjanethcon@gmail.com>
2015-08-18 17:14:09 -0500

    se agregan los cards al recycledview y con un adaptador se llenan de información

--- app/src/main/java/com/example/lapicher/thefm/ui/HypedArtistsFragment.java
+++ app/src/main/java/com/example/lapicher/thefm/ui/HypedArtistsFragment.java
@@ -10,6 +10,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.lapicher.thefm.R;
+import com.example.lapicher.thefm.domain.Artist;
+import com.example.lapicher.thefm.ui.Adapter.HypedArtistAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by GKN on 13/08/2015.
@@ -17,9 +21,16 @@ import com.example.lapicher.thefm.R;
 public class HypedArtistsFragment extends Fragment {
 
     public static final String LOG_TAG=HypedArtistsFragment.class.getName();
+    public static final int NUM_COLUMNS=2;
 
     private RecyclerView mHypedArtistList;
-    public static final int NUM_COLUMNS=2;
+    private HypedArtistAdapter adapter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        adapter=new HypedArtistAdapter(getActivity());
+    }
 
     @Nullable
     @Override
@@ -32,10 +43,23 @@ public class HypedArtistsFragment extends Fragment {
          */
         mHypedArtistList=(RecyclerView)root.findViewById(R.id.hyped_artists_list);
 
+        setupArtistsList();
+        setDummyContent();
+
         return root;
     }
     private void setupArtistsList()
     {
         mHypedArtistList.setLayoutManager(new GridLayoutManager(getActivity(),NUM_COLUMNS));
+        mHypedArtistList.setAdapter(adapter);
+    }
+    private void setDummyContent()
+    {
+        ArrayList<Artist> artists=new ArrayList<>();
+        for (int i=0;i<10;i++)
+        {
+            artists.add(new Artist("Artist "+i));
+        }
+        adapter.addAll(artists);
     }
 }
