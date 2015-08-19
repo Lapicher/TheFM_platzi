0e8bc7af86569a0a33487466735f39c68b7a72bc
Nataly Contreras <natalyjanethcon@gmail.com>
2015-08-19 17:18:05 -0500

    se agrega retrofit para conectar con API Last-FM

---
+++ app/src/main/java/com/example/lapicher/thefm/io/ApiConstantes.java
@@ -0,0 +1,8 @@
+package com.example.lapicher.thefm.io;
+
+/**
+ * Created by Nataly on 19/08/2015.
+ */
+public class ApiConstantes {
+    public static final String URL_BASE="";
+}
---
+++ app/src/main/java/com/example/lapicher/thefm/io/LastFMApiService.java
@@ -0,0 +1,7 @@
+package com.example.lapicher.thefm.io;
+
+/**
+ * Created by Nataly on 19/08/2015.
+ */
+public interface LastFMApiService {
+}
